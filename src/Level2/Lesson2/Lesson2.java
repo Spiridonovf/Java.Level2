25b006cf003d31c1ea53d59f7e54adce57c28719
Spiridonovf <spiridonovf@yandex.ru>
2020-05-20 13:23:00 +0300

    lev2

---
+++ src/Level2/Lesson2/Lesson2.java
@@ -0,0 +1,13 @@
+package Level2.Lesson2;
+
+public class Lesson2 {
+
+    public static void main(String[] args) {
+        //String arr[]=new ;
+        //SummArray();
+    }
+
+    private static void SummArray(String arr[][]) {
+    }
+
+}
